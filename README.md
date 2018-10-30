8b60db1d86bac7b71985f73c83850829f83b5424
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-30 15:26:48 +0300

    Setup project

---
+++ README.md
@@ -0,0 +1,5 @@
+# File Server
+
+Project: File Server
+
+Go to the *src* directory, change *Client.java*, *Server.java* and put your *.java* file(s) in it.
---
+++ src/fileserver/Server.java
@@ -0,0 +1,7 @@
+package fileserver;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Server started!");
+    }
+}
