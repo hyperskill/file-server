8f1aabe98192af1ed5b2caf04053dc08a0db7683
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-01-29 21:35:08 +0200

    Create a Server and Client

--- src/fileserver/Client.java
+++ src/fileserver/Client.java
@@ -1,7 +1,31 @@
 package fileserver;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 public class Client {
-    public static void main(String[] args) throws Exception {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
+
+    public static void main(String[] args) throws IOException{
         System.out.println("Client started!");
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Sent: ");
+            String msg = scanner.nextLine();
+
+            output.writeUTF(msg); // sending message to the server
+            String receivedMsg = input.readUTF(); // response message
+
+            System.out.println("Received: " + receivedMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
